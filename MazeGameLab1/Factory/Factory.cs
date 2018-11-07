76b4315eb2d2ae36fd3cb253adc76fd4aff576e1
robtam <robertas.tamasauskas@ktu.edu>
2018-11-07 19:49:48 +0200

    create initial monster setup, factory

--- MazeGameLab1/Factory/Factory.cs
+++ MazeGameLab1/Factory/Factory.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using MazeGameLab1.Global;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MazeGameLab1.Factory
 {
-    class Factory
+    abstract class Factory
     {
+        public abstract Monster CreateEnemy(string type, int posx, int posy, int h, int s, int dist, int dam, int dr, bool isd);
     }
 }
