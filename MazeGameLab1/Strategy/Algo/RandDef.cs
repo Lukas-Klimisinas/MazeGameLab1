99f1ebd81155e07d03bcf5e34b25979d8c53c421
Lukas Klimišinas <klimisinas.lukas@gmail.com>
2018-11-09 10:22:59 +0200

    Strategy

---
+++ MazeGameLab1/Strategy/Algo/RandDef.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeGameLab1.Strategy
+{
+    class RandDef : MovementAlgorithm
+    {
+        public override void Move()
+        {
+            Console.WriteLine($"I'm moving random ({this.GetHashCode()})");
+        }
+    }
+}
