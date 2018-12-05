a90e17e44010e18b5321180093fb6e903b7a44df
Vytas <vytas.vadapolas@ktu.edu>
2018-12-05 19:53:55 +0200

    Added Proxy pattern

---
+++ MazeGameLab1/Proxy/EndDisplay.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MazeGameLab1.Proxy
+{
+    public class EndDisplay : Display
+    {
+
+        string Dispath;
+   
+        public EndDisplay(string path)
+        {
+            Dispath = path;
+            
+            try
+            {
+                Bitmap image = new Bitmap(path,true);
+                
+            }
+            catch (Exception ex)
+            {
+                
+                throw new ApplicationException("image path not found", ex);
+            }
+        }
+
+        
+
+
+        public void ShowDisplay()
+        {
+
+            Console.WriteLine("Display "+ Dispath +" loaded");
+        }
+    }
+}
---
+++ MazeGameLab1/Proxy/EndDisplayProxy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MazeGameLab1.Proxy
+{
+    class EndDisplayProxy : Display
+    {
+        Display display;
+
+        string dispath;
+        Display parentProxy;
+
+        public EndDisplayProxy(string path)
+        {
+            dispath = path;
+        }
+
+        public EndDisplayProxy(Display anotherProxy)
+        {
+            parentProxy = anotherProxy;
+        }
+
+        public void ShowDisplay()
+        {
+            if (parentProxy == null)
+            {
+                if (display==null)
+                {
+                    display = new EndDisplay(dispath);
+                } 
+                display.ShowDisplay();
+            }
+            else
+            {
+                parentProxy.ShowDisplay();
+            }
+            
+        }
+
+    }
+}
